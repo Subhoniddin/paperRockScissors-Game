91fc1c835acd25797b25be78eb17e8073ca17428
Subhoniddin <subhoniddin571@gmail.com>
2025-04-05 17:26:31 +0500

    game

---
+++ src/js/htmlElaments.js
@@ -0,0 +1,18 @@
+
+const choicesElement = document.getElementById('choices');
+const playingZone = document.getElementById('playingZone');
+const userChoiceDiv = document.getElementById('userChoice');
+const userChoiceImg = document.getElementById('userChoiceImg');
+const computerChoiceDiv = document.getElementById('computerChoice');
+const computerChoiceImg = document.getElementById('computerChoiceImg');
+const resultElement = document.getElementById('result');
+const scoreElement = document.getElementById('score');
+const playAgainBtn = document.getElementById('playAgain');
+const advancedBtn = document.getElementById('advancedBtn');
+const rulesBtn = document.getElementById('rulesBtn');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('closeModal');
+const rulesImage = document.getElementById('rulesImage');
+const choiceItems = choicesElement.querySelectorAll('li')
+
+export {choicesElement, playingZone, userChoiceDiv, userChoiceImg, computerChoiceDiv, computerChoiceImg, resultElement, scoreElement, playAgainBtn, advancedBtn, rulesBtn, modal, closeModal, rulesImage, choiceItems}
